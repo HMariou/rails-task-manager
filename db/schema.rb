29bdb6c590cbbbd9438a5f8bcc1bc63c6cb4fe8d
Hugo Mariou <mariou.hugo@gmail.com>
2022-08-09 16:14:20 -0300

    in progress

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_09_155206) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_09_180359) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
-    t.string "string"
-    t.string "details"
-    t.string "text"
-    t.string "completed"
-    t.string "boolean"
+    t.text "details"
+    t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
